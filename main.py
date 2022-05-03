775c1c2d28b863236a0731bc46fe0f4d214cd9ac
cds30895 <86209895+cds30895@users.noreply.github.com>
2022-05-03 14:06:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Parking Tracker
+------
+This program is to track the license plates of cars that park in my spot, the date each occurred, and if it lasted all day.
+"""
+from csv import writer
+from datetime import datetime
+import reader
+
+ # Use writer() object from csv library to write 
+# the data to a new row in the data sheet
+def write_new_row(file, new_data):
+  with open(file, "a", newline="") as data:
+    writer_object = writer(data)
+    writer_object.writerow(new_data)
+    data.close()
+
+if __name__ == "__main__":
+
+  file = "parking_data.csv"
+  date = datetime.today().strftime("%m/%d/%Y")
+
+  # Input or view data?
+  user_choice = int(input("Would you like to:\n 1. Input new data\n 2. View existing data\n"))
+
+  if user_choice == 2:
+    reader.count_plates(file)
+
+  elif user_choice == 1:
+    
+    # Gather input data
+    license_plate = input("Input plate number: ").upper()
+    student = input("Student parking tag? (leave blank if none) Y/N: ").lower()
+    am = input("AM? y/n: ").lower()
+    pm = input("PM? y/n: ").lower()
+  
+    # Collect data in a list
+    new_data = [date, license_plate, student, am, pm]
+  
+    # Write data to csv
+    write_new_row(file, new_data)
+  
+    # Print a count of unique plates
+    reader.count_plates(file)
+    # reader.count_students(file)
---
+++ reader.py
@@ -0,0 +1,51 @@
+"""
+Reader
+-------
+This is a space for functions
+that manipulate data from csv
+files to display statistics of
+parking violations.
+"""
+
+from csv import reader
+
+def count_plates(file):
+  with open(file,"r", newline="") as data:
+    reader_object = reader(data)
+    plates = []
+    unique_plates = []
+    # List all plate names from data sheet
+    for row in reader_object:
+      plates.append(row[1])
+  # List all unique plates
+  for plate in plates:
+    if plate not in unique_plates and plate != "license plate" and plate != "":
+      unique_plates.append(plate)
+  # For each unique plate, count and print 
+  # the occurrences in the data sheet
+  print()
+  for plate in unique_plates:
+    print(plate + ": " + str(plates.count(plate)))
+  data.close()
+
+def count_students(file):
+  with open(file, "r", newline="") as data:
+    reader_object = reader(data)
+    responses = []
+    # Put all responses into a list to be counted
+    for row in reader_object:
+      responses.append(row[2])
+    print(responses)
+    students = responses.count("y")
+    staff = responses.count("n")
+    for i in responses:
+      if i == "student":
+        del(i)
+    print(responses)
+    unknown = len(responses)
+    
+    print()
+    print("Students: " + str(students))
+    print("Staff: " + str(staff))
+    print("Unknown: " + str(unknown))
+  data.close()
